45678ded409d32814172f9908597500917eb0aea
psjsaravana <psj.saravana@gmail.com>
2015-08-13 00:39:36 +0530

    code added

---
+++ android/src/com/psj/saravana/myfirsatanimation/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.psj.saravana.myfirsatanimation.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.psj.saravana.myfirsatanimation.MyAnimation;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyAnimation(), config);
+	}
+}
